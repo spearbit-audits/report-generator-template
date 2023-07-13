cf958fb64318f98afeadc0c571d600f20000cda1
Giovanni Di Siena <giodisiena@gmail.com>
2023-07-13 12:10:06 +0100

    Add update script (#5)

---
+++ scripts/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is run from the toplevel of the working tree.
+
+git subtree pull --prefix cyfrin-report/report-generator-template https://github.com/Cyfrin/report-generator-template main --squash

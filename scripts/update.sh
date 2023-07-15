e481e7e01939fd028a9b6a0f56986ae0847b9f9e
Giovanni Di Siena <giodisiena@gmail.com>
2023-07-15 13:58:28 +0200

    Copy workflow changes (#8)

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,3 +3,4 @@
 # This is run from the toplevel of the working tree.
 
 git subtree pull --prefix cyfrin-report/report-generator-template https://github.com/Cyfrin/report-generator-template main --squash
+cp cyfrin-report/report-generator-template/.github/workflows/main.yml .github/workflows/main.yml
